0a5a8e16718fb40f4c72f4828ec12195fcd7bf8d
Pablo Mercado <odacremolbap@gmail.com>
2017-04-18 18:09:10 +0200

    add spoil instructions

---
+++ demo/step4/spoil.sh
@@ -0,0 +1,5 @@
+# this url will make the CPU go crazy
+http://spclvz8cv5.stackpoint.io/spoil
+
+# after hiting that URL the hpa will notice (30 seconds later aprox)
+# and will tell the controller manager to bring a new pod (30 seconds more maybe)
